6208c0d02cef9617d38054816643344e3ea11216
qy6020 <70052654+qy6020@users.noreply.github.com>
2020-08-22 11:33:19 +0000

    Deploying to gh-pages from  @ 61cc632a184762ae6a1049ccb6ffab7882c5b0bb 🚀

---
+++ precache-manifest.2a49e049c0baf606f564e6ed726331af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6661aa5697c062e80028d7ab18e5917",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f23de669dd38b08da548.bundle.js"
+  },
+  {
+    "url": "runtime~main.f23de669dd38b08da548.bundle.js"
+  },
+  {
+    "url": "vendors~main.f23de669dd38b08da548.bundle.js"
+  },
+  {
+    "url": "vendors~main.f23de669dd38b08da548.bundle.js.LICENSE.txt"
+  }
+]);
